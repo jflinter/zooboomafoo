ae9c16c67cb9dcad13666475835b8fec3febea6b
Jack Flintermann <jack@stripe.com>
2020-03-09 12:28:18 -0400

    fix things

--- zooboomafoo/main.m
+++ zooboomafoo/main.m
@@ -68,7 +68,7 @@ NSDictionary* setupZoom(NSRunningApplication *zoom) {
     for (int i = 0; i < 10; i++) {
         postKeyCode(kVK_UpArrow, zoom, NO, settingsWindow); // move to top
     }
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         postKeyCode(kVK_DownArrow, zoom, NO, settingsWindow); // move to virtual background
     }
     postKeyCode(kVK_Return, zoom, NO, settingsWindow); // select virtual background
@@ -95,7 +95,58 @@ void loadContentsForFrame(int frame, NSString *frameDirectory, NSArray *zoomFile
     }
 }
 
+NSData *sendSynchronousGetRequest(NSURL *url, __autoreleasing NSError **errorPtr) {
+    dispatch_semaphore_t    sem;
+    __block NSData *        result;
+
+    result = nil;
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    sem = dispatch_semaphore_create(0);
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request
+        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (errorPtr != NULL) {
+            *errorPtr = error;
+        }
+        if (error == nil) {
+            result = data;
+        }
+        dispatch_semaphore_signal(sem);
+    }] resume];
+
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+
+   return result;
+}
+
+// destinationDir == e.g. zoom/tmp
 NSTimeInterval unpack(NSString *gifPath, NSString *destinationDir) {
+    // first, if the tmpdir does not exist, make it
+    if (![[NSFileManager defaultManager] fileExistsAtPath:destinationDir]) {
+        [[NSTask launchedTaskWithLaunchPath:@"/bin/mkdir" arguments:@[destinationDir]] waitUntilExit];
+        NSString *downloadDir = [destinationDir stringByAppendingPathComponent:@"/downloads"];
+        [[NSTask launchedTaskWithLaunchPath:@"/bin/mkdir" arguments:@[downloadDir]] waitUntilExit];
+    }
+    
+    // if the provided path is a URL, download it to a downloads folder
+    NSURL *gifURL = [NSURL URLWithString:gifPath];
+    if (gifURL && [gifURL.scheme isEqualToString:@"https"]) {
+        NSString *hash = @(gifURL.hash).description;
+        // zoom/downloads/{hash}.gif
+        NSString *filename = [NSString stringWithFormat:@"downloads/%@.gif", hash];
+        gifPath = [destinationDir stringByAppendingPathComponent:filename];
+        
+        NSError *error;
+        NSData *result = sendSynchronousGetRequest(gifURL, &error);
+        if (error != nil) {
+            return 0;
+        }
+        [result writeToFile:gifPath atomically:YES];
+    }
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:gifPath]) {
+        return 0;
+    }
+    
     NSString *filename = [gifPath lastPathComponent];
     NSString *destinationFilename = [destinationDir stringByAppendingPathComponent:filename];
     
@@ -118,9 +169,12 @@ NSTimeInterval unpack(NSString *gifPath, NSString *destinationDir) {
     if ([[NSFileManager defaultManager] fileExistsAtPath:destinationFilename]) {
         return duration; // cache the gif in the tmpdir to save time
     }
-    [[NSTask launchedTaskWithLaunchPath:@"/bin/rm" arguments:@[@"-rf", destinationDir]] waitUntilExit];
-    [[NSTask launchedTaskWithLaunchPath:@"/bin/mkdir" arguments:@[destinationDir]] waitUntilExit];
-    [[NSTask launchedTaskWithLaunchPath:@"/usr/local/bin/convert" arguments:@[gifPath, @"-coalesce", [destinationDir stringByAppendingString:@"/%05d.png"]]] waitUntilExit];
+    
+    NSString *framesDir = [destinationDir stringByAppendingPathComponent:@"/frames"];
+    [[NSTask launchedTaskWithLaunchPath:@"/bin/rm" arguments:@[@"-rf", framesDir]] waitUntilExit];
+    [[NSTask launchedTaskWithLaunchPath:@"/bin/mkdir" arguments:@[framesDir]] waitUntilExit];
+    
+    [[NSTask launchedTaskWithLaunchPath:@"/usr/local/bin/convert" arguments:@[gifPath, @"-coalesce", [framesDir stringByAppendingString:@"/%05d.png"]]] waitUntilExit];
     [[NSFileManager defaultManager] copyItemAtPath:gifPath toPath:destinationFilename error:nil];
     return duration;
 }
@@ -149,12 +203,6 @@ int main(int argc, const char * argv[]) {
             return 1;
         }
 
-        NSString *GIF_PATH = [args[1] stringByStandardizingPath];
-        if (![[NSFileManager defaultManager] fileExistsAtPath:GIF_PATH]) {
-            NSLog(@"There doesn't seem to be a GIF there.");
-            return 1;
-        }
-
         if (![[NSFileManager defaultManager] fileExistsAtPath:@"/usr/local/bin/convert"]) {
             NSLog(@"You don't seem to have imagemagick installed. Please run `brew install imagemagick`.");
             return 1;
@@ -165,7 +213,7 @@ int main(int argc, const char * argv[]) {
         } else {
             ZOOM_DIR = [[@"~/Library/Application Support/zoom.us/data/VirtualBkgnd_Default" stringByStandardizingPath] stringByReplacingOccurrencesOfString:@" " withString:@"\ "];
         }
-        NSString *ZOOM_TMPDIR = [ZOOM_DIR stringByAppendingPathComponent:@"tmp"];
+        NSString *ZOOM_TMPDIR = [ZOOM_DIR stringByAppendingPathComponent:@"zooboomafoo"];
         NSDirectoryEnumerator *enumerator = [[NSFileManager defaultManager] enumeratorAtURL:[NSURL fileURLWithPath:ZOOM_DIR] includingPropertiesForKeys:@[NSURLNameKey, NSURLIsDirectoryKey] options:NSDirectoryEnumerationSkipsSubdirectoryDescendants|NSDirectoryEnumerationSkipsHiddenFiles errorHandler:nil];
         NSMutableArray *ZOOM_IMAGE_PATHS = [NSMutableArray array];
         for (NSURL *fileURL in enumerator) {
@@ -177,9 +225,22 @@ int main(int argc, const char * argv[]) {
                 [ZOOM_IMAGE_PATHS addObject:fileURL.path];
             }
         }
+        
+        if (ZOOM_IMAGE_PATHS.count < 2) {
+            NSLog(@"Hrmm, there aren't enough images in your zoom directory. Ping jack for help.");
+            return 1;
+        }
 
+        NSString *GIF_PATH = [args[1] stringByStandardizingPath];
+        
         CGFloat duration = unpack(GIF_PATH, ZOOM_TMPDIR);
-        NSUInteger frameCount = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:ZOOM_TMPDIR error:nil] count];
+        if (duration == 0) {
+            NSLog(@"There doesn't seem to be a GIF there.");
+            return 0;
+        }
+        NSString *framesDir = [ZOOM_TMPDIR stringByAppendingPathComponent:@"/frames"];
+        
+        NSUInteger frameCount = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:framesDir error:nil] count];
 
         setupZoom(zoom);
         
@@ -198,10 +259,14 @@ int main(int argc, const char * argv[]) {
         dispatch_resume(source);
 
         for (int frame = 0; frame < frameCount && looping; frame = (frame + 1) % frameCount) {
+            if (zoom.isTerminated) {
+                scrollZoomToTheLeft(zoom, settingsWindow);
+                exit(0);
+            }
             if (frame == 0) {
                 scrollZoomToTheRight(zoom, settingsWindow);
             }
-            loadContentsForFrame(frame, ZOOM_TMPDIR, ZOOM_IMAGE_PATHS);
+            loadContentsForFrame(frame, framesDir, ZOOM_IMAGE_PATHS);
             toggleFrame(frame, zoom, settingsWindow);
             [NSThread sleepForTimeInterval:duration];
         }
